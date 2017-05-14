9f1a22fbfd867df9eacfdd52e9bde2e4b4ac267c
Ladis72 <Ladispascual@gmail.com>
2017-05-14 20:04:00 +0200

    Compras clientes

--- clientes.h
+++ clientes.h
@@ -34,11 +34,22 @@ private slots:
 
     void on_lineEditCod_editingFinished();
 
+    void on_radioButtonMeses_clicked();
+
+    void on_radioButtonAnos_clicked();
+
+    void on_radioButtonFechas_clicked();
+
+    void on_dateEditHasta_userDateChanged(const QDate &date);
+
+    void on_dateEditDesde_userDateChanged(const QDate &date);
+
 private:
     Ui::Clientes *ui;
 
     baseDatos base;
     QSqlQueryModel *modeloTabla;
+    QSqlQueryModel modeloCompras;
     QDataWidgetMapper mapper;
     QSqlQuery consulta;
 
@@ -47,6 +58,7 @@ private:
     void borrarFormulario();
     bool eventFilter(QObject *obj, QEvent *event);
     void refrescarBotones(int i);
+    void cargarCompras();
     QStringList recogerDatosFormulario();
 
 };
